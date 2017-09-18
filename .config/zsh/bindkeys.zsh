2c22e1877fb253532ae6d22cbb9f8a4305361c37
Miguel Segade <miguelrsegade@gmail.com>
2017-09-19 00:18:43 +0200

    Cleaned fzf-cd-widget, is included in fzf package

--- .config/zsh/bindkeys.zsh
+++ .config/zsh/bindkeys.zsh
@@ -1,20 +1,4 @@
 bindkey "^R" history-incremental-search-backward
-fzf-cd-widget() {
-  local cmd="${FZF_ALT_C_COMMAND:-"command find -L . -mindepth 1 \\( -path '*/\\.*' -o -fstype 'sysfs' -o -fstype 'devfs' -o -fstype 'devtmpfs' -o -fstype 'proc' \\) -prune \
-    -o -type d -print 2> /dev/null | cut -b3-"}"
-  setopt localoptions pipefail 2> /dev/null
-  local dir="$(eval "$cmd" | FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-40%} --reverse $FZF_DEFAULT_OPTS $FZF_ALT_C_OPTS" $(__fzfcmd) +m)"
-  if [[ -z "$dir" ]]; then
-    zle redisplay
-    return 0
-  fi
-  cd "$dir"
-  local ret=$?
-  zle reset-prompt
-  typeset -f zle-line-init >/dev/null && zle zle-line-init
-  return $ret
-}
-zle     -N    fzf-cd-widget
 bindkey "^F" fzf-cd-widget
 
 # Enable Ctrl-x-e to edit command line
