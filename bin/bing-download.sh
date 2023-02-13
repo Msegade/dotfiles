52f4d1d2f2d0c93105e57b6b83b929f836878f23
Msegade <miguelrsegade@gmail.com>
2023-02-13 09:20:37 +0100

    Bing download

--- bin/bing-download.sh
+++ bin/bing-download.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
 
+# Command line option
+if [ "$#" -eq 1 ]; then
+    idx=$(date -d "$1" +%s)
+    dateString=$1
+else
+    idx=0
+    dateString=$(date +%F)
+fi
+
+
+
 # Define the URL of the Bing image of the day
-bing="https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1"
+bing="https://www.bing.com/HPImageArchive.aspx?format=js&idx=$idx&n=1"
+echo $bing
 
 # Fetch the JSON data from the URL
 json=$(curl -s "$bing")
+if [ "$(echo "$json" | grep -c '"images":\[\]')" -eq 1 ]; then
+  echo "Error: No image found for the specified date."
+  exit 1
+fi
 
 # Extract the URL of the image from the JSON data
 url=$(echo "$json" | grep -o '"url":"[^"]*"' | sed 's/"url":"\([^"]*\)"/\1/')
@@ -17,6 +33,6 @@ description=$(echo "$json" | grep -o '"copyright":"[^"]*"' | sed 's/"copyright":
 
 # Download the image
 outputDir=$HOME/Images/wallpapers
-curl -s -o $outputDir/"$(date +%F).jpg" "$url"
-echo "$description" > $outputDir/"$(date +%F).txt"
+curl -s -o $outputDir/"$dateString.jpg" "$url"
+echo "$description" > $outputDir/"$dateString.txt"
 
